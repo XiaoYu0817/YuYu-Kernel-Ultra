ce1e2ee70014bab7c5eeaa3c8c411f81b5559b3a
XiaoYu0817 <xiaoyu0817@users.noreply.github.com>
2026-08-15 20:12:53 +0800

    Add missing MaterialTheme import in SettingsScreen

--- app/apk-ng/src/main/java/com/topjohnwu/magisk/ui/settings/SettingsScreen.kt
+++ app/apk-ng/src/main/java/com/topjohnwu/magisk/ui/settings/SettingsScreen.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
